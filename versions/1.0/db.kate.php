1c4362b210088f7c23d197647162b0c5ffdb48d4
perez <pcx1256@gmail.com>
2014-03-22 14:44:16 -0700

    Create db.kate.php

---
+++ versions/1.0/db.kate.php
@@ -0,0 +1,9 @@
+<?php
+// (c)Perez Karjee(www.aas9.in)
+// Project Site www.aas9.in/zerocms
+// Created March 2014
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'dbusername');
+define('MYSQL_PASSWORD', 'dbpassword');
+define('MYSQL_DB', 'dbname');
+?>
